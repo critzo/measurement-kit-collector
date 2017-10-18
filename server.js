847dda9c7254b15811ed3783c27613289358ce58
Chris Ritzo <critzo@opentechinstitute.org>
2017-10-18 14:27:11 -0400

    some local customizations for my fork

--- server.js
+++ server.js
@@ -6,9 +6,13 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const uuid = require("uuid4");
-
+const fs = require("fs");
 const app = express();
 app.use(bodyParser.json({limit: "50mb"}));
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
 
 app.post("/report/", (request, response) => {
     console.log("OPEN");
@@ -22,6 +26,10 @@ app.post("/report/:reportId", (request, response) => {
     console.log("UPDATE");
     console.log(request.params.reportId);
     //console.log(request.body);
+    filePath = __dirname + '/report/:reportId';
+    fs.appendFile(filePath, request.body, function () {
+        respond.end();
+    });
     response.json({});
 });
 
